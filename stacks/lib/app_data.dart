58811b3c1c57392305897f37caf0eaac8f691ead
cristian <calvarezmartinez.cf@iesesteveterrades.com>
2023-10-10 19:57:39 +0200

    practica

---
+++ stacks/lib/app_data.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppData with ChangeNotifier {
+// App status
+  bool isSidebarLeftVisible = false;
+
+  bool isSidebarRightVisible = false;
+
+// Toggle sidebar left
+  void toggleSidebarLeft() {
+    isSidebarLeftVisible = !isSidebarLeftVisible;
+    notifyListeners();
+  }
+
+  void toggleSidebarRight() {
+    isSidebarRightVisible = !isSidebarRightVisible;
+    notifyListeners();
+  }
+}
---
+++ stacks/lib/layout_part_central.dart
@@ -0,0 +1,94 @@
+import 'package:cupertino_base/widget_popover.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'app_data.dart';
+
+class LayoutPartCentral extends StatefulWidget {
+  const LayoutPartCentral({super.key});
+
+  @override
+  LayoutPartCentralState createState() => LayoutPartCentralState();
+}
+
+class LayoutPartCentralState extends State<LayoutPartCentral> {
+  final GlobalKey _settingsButtonKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    AppData appData = Provider.of<AppData>(context);
+
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          leading: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CupertinoButton(
+                  padding: const EdgeInsets.all(0.0),
+                  onPressed: appData.toggleSidebarLeft,
+                  child: Icon(
+                    CupertinoIcons.sidebar_left,
+                    color: appData.isSidebarLeftVisible
+                        ? CupertinoColors.activeBlue
+                        : CupertinoColors.black,
+                    size: 24.0,
+                    semanticLabel: 'Text to announce in accessibility modes',
+                  )),
+              const SizedBox(width: 16),
+            ],
+          ),
+          middle: CupertinoButton(
+            key: _settingsButtonKey,
+            padding: const EdgeInsets.all(0.0),
+            onPressed: () {
+              WidgetPopover.showPopover(
+                  context: context,
+                  key: _settingsButtonKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            print("Opció 1 premuda");
+                            WidgetPopover.hidePopover();
+                          },
+                          child: Text('Opció 1'),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            print("Opció 2 premuda");
+                            WidgetPopover.hidePopover();
+                          },
+                          child: Text('Opció 2'),
+                        ),
+                      ],
+                    ),
+                  ));
+            },
+            child: const Icon(
+              CupertinoIcons.settings,
+              color: CupertinoColors.black,
+              size: 24.0,
+              semanticLabel: 'Text to announce in accessibility modes',
+            ),
+          ),
+          trailing: CupertinoButton(
+              padding: const EdgeInsets.all(0.0),
+              onPressed: appData.toggleSidebarRight,
+              child: Icon(
+                CupertinoIcons.sidebar_right,
+                color: appData.isSidebarRightVisible
+                    ? CupertinoColors.activeBlue
+                    : CupertinoColors.black,
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              )),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text("Central")],
+          ),
+        ));
+  }
+}
